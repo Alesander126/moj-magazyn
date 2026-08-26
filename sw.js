bc61bf02f0bd296dda05d7d5ada2d7cbdad493d0
Alesander126 <mateusz.s.info@gmail.com>
2026-08-26 18:22:32 +0200

    Aktualizacja pamięci offline aplikacji

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='moj-magazyn-1';
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest']))));
+const CACHE='moj-magazyn-2';
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest','https://unpkg.com/html5-qrcode@2.3.8/html5-qrcode.min.js']))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
 

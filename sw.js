759debd9f13269967e5fcdd15e91ebd7cd26ddc9
Alesander126 <mateusz.s.info@gmail.com>
2026-08-26 18:34:27 +0200

    Wymuszenie aktualizacji aplikacji na iPhonie

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const CACHE='moj-magazyn-2';
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest','https://unpkg.com/html5-qrcode@2.3.8/html5-qrcode.min.js']))));
+const CACHE='moj-magazyn-3';
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest','https://unpkg.com/html5-qrcode@2.3.8/html5-qrcode.min.js'])).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
 

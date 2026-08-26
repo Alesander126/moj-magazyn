2df8b6f3e7c848d1e412f3ac9a60da7a33081bb2
Alesander126 <mateusz.s.info@gmail.com>
2026-08-26 18:48:50 +0200

    Natychmiastowe pobieranie aktualnej wersji aplikacji

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='moj-magazyn-3';
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest','https://unpkg.com/html5-qrcode@2.3.8/html5-qrcode.min.js'])).then(()=>self.skipWaiting())));
+const CACHE='moj-magazyn-4',APP=['./','./index.html','./manifest.webmanifest','https://unpkg.com/html5-qrcode@2.3.8/html5-qrcode.min.js'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(APP)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+self.addEventListener('fetch',e=>{if(e.request.mode==='navigate'){e.respondWith(fetch(e.request).catch(()=>caches.match('./index.html')));return}e.respondWith(caches.match(e.request).then(x=>x||fetch(e.request)))});
 
